0c58e35617d846acef0039f3ca81786f92a312b7
yanmingk <75762362+yanmingk@users.noreply.github.com>
2021-07-14 02:49:35 -0400

    Add bagging

--- zh_dataset.py
+++ zh_dataset.py
@@ -1,6 +1,7 @@
 import torch
 import json
 import os
+import numpy as np
 
 class ZhEnDataset(torch.utils.data.IterableDataset):
     def __init__(self, split, start, end):
@@ -29,3 +30,30 @@ class ZhEnDataset(torch.utils.data.IterableDataset):
 
     def __len__(self):
         return self.end - self.start
+
+
+class BaggingDataset(torch.utils.data.IterableDataset):
+    def __init__(self, size):
+        super(ZhEnDataset).__init__()
+
+        self.size = size
+
+        # for training only
+        _file = os.path.join(".", "translation2019zh", "translation2019zh_train.json")
+
+        # load the whole training set
+        self.pairs = []
+        for i, line in enumerate( open(_file, mode='r', encoding='utf-8')):
+            d = json.loads(line)
+            self.pairs.append( (d['english'] + '\n', d['chinese'] + '\n') )
+
+        self.pairs_sampled = []
+
+    def resample(self):
+        self.pairs_sampled = [self.pairs[i] for i in np.random.choice(len(self.pairs), size=self.size, replace=True, p=None)]
+
+    def __iter__(self):
+        return iter(self.pairs_sampled)
+
+    def __len__(self):
+        return self.size
